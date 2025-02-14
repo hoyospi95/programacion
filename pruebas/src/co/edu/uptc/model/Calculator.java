90cbf41bfe4351ad6785bc715429ffc15d4b1443
hoyospi95 <124736836+hoyospi95@users.noreply.github.com>
2025-02-14 07:26:20 -0500

    Add files via upload

---
+++ pruebas/src/co/edu/uptc/model/Calculator.java
@@ -0,0 +1,12 @@
+package co.edu.uptc.model;
+
+public class Calculator {
+
+    public int add(int a, int b){}
+
+    public int substract(int a, int b){}
+
+    public int multiply(int a, int b){}
+
+    public int divide(int a, int b){}
+}
