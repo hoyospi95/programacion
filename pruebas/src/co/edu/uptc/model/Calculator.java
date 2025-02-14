0074a2a317db8a741f52e0035a9a2ccbbf6206ad
unknown <jorge.hoyos>
2025-02-14 07:38:59 -0500

    primer commit

--- pruebas/src/co/edu/uptc/model/Calculator.java
+++ pruebas/src/co/edu/uptc/model/Calculator.java
@@ -2,7 +2,9 @@ package co.edu.uptc.model;
 
 public class Calculator {
 
-    public int add(int a, int b){}
+    public int add(int a, int b){
+        return a + b;
+    }
 
     public int substract(int a, int b){}
 
