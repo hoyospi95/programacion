4700428d73565372a979f91fdcbf0d2c68f7f8a5
unknown <jorge.hoyos>
2025-02-14 07:49:48 -0500

    segundo intento

--- pruebas/src/co/edu/uptc/model/Calculator.java
+++ pruebas/src/co/edu/uptc/model/Calculator.java
@@ -8,7 +8,9 @@ public class Calculator {
 
     public int substract(int a, int b){}
 
-    public int multiply(int a, int b){}
+    public int multiply(int a, int b){
+        return a + b;
+    }
 
     public int divide(int a, int b){}
 }
